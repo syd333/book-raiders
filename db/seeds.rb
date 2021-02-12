353819b010bc482970f8b7a7debbfa1f9c0c5159
syd333 <sydmiranda@icloud.com>
2021-02-11 16:21:52 -0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#need Destory.all
+# RATING = rand(1..5)
+
+10.times do 
+    User.create(name: Faker::Name.name)
+end
+
+20.times do 
+    Author.create(name: Faker::Book.author)
+end
+
+10.times do 
+    Genre.create(name: Faker::Book.genre)
+end
+
+20.times do
+    Book.create(title: Faker::Book.title, author: Author.all.sample, genre: Genre.all.sample)
+end
+
