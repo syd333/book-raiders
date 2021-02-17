75b60e1252548f88a937e59e1ca3a2e4ba824062
syd333 <sydmiranda@icloud.com>
2021-02-17 07:18:34 -0800

    first draft navbar, installed bootstrap

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.1'
 
+gem 'bootstrap', '~> 4.0.0'
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.1.1'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2021_02_16_180808) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "books", "authors"
