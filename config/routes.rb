4a37a88e5d0afdc14a73d1153a44ccaf656844c9
syd333 <sydmiranda@icloud.com>
2021-02-18 10:46:24 -0800

    front-end css & bootstrap, first draft homepage, fixed lougout bug routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  get 'homepage/home'
   # get '/login', to: 'sessions#new'
   # post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   delete '/logout', to: 'sessions#destroy'
+
   resources :books do 
     resources :reviews, shallow: true 
   end
+
   resources :reviews
-  resources :books
-  resources :genres
-  resources :authors
+  resources :books, only: [:index, :show]
+  resources :genres, only: [:index, :show]
+  resources :authors, only: [:index, :show]
   resources :users
-  resources :sessions 
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
