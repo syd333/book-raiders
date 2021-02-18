e1c7a779206a5ec154da70dc710417d0ba6db5be
syd333 <sydmiranda@icloud.com>
2021-02-17 17:15:06 -0800

    i dont know

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,5 +3,4 @@ class Author < ApplicationRecord
     has_many :books
     has_many :genres, through: :books 
 
-    validates :author, presence: true 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,5 +3,4 @@ class Genre < ApplicationRecord
     has_many :books
     has_many :authors, through: :books 
 
-    validates :genre, presence: true 
 end
